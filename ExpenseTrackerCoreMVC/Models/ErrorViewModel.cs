1f0d0361f2b62b99ae33e56b455ca707fccb5c46
Deon <deonsadcopen@outlook.com>
2025-07-17 11:16:02 +0800

    Add project files.

---
+++ ExpenseTrackerCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpenseTrackerCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
