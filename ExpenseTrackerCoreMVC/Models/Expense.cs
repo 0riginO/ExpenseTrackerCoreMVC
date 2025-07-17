a351ea0cda3a9d55c2f2872047e5d0ce75bb1d0f
Deon <deonsadcopen@outlook.com>
2025-07-17 13:44:51 +0800

    Add Expense class to Models.

---
+++ ExpenseTrackerCoreMVC/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTrackerCoreMVC.Models
+{
+	public class Expense
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public decimal Amount { get; set; }
+		public string Category { get; set; } = string.Empty;
+		public DateTime DateAdded { get; set; } = DateTime.Now;
+	}
+}
