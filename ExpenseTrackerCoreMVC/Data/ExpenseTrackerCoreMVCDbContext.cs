662d326f252126b4a7bbcafc25c8d0a7ad12229f
Deon <deonsadcopen@outlook.com>
2025-07-22 15:48:20 +0800

    Add DbContext file.

---
+++ ExpenseTrackerCoreMVC/Data/ExpenseTrackerCoreMVCDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTrackerCoreMVC.Models;
+
+namespace ExpenseTrackerCoreMVC.Data
+{
+	public class ExpenseTrackerCoreMVCDbContext : DbContext
+	{
+		public ExpenseTrackerCoreMVCDbContext(DbContextOptions<ExpenseTrackerCoreMVCDbContext> options) : base(options)
+		{ 
+		}
+
+		public DbSet<Expense> Expenses { get; set; }
+		public DbSet<Category> Categories { get; set; }
+
+
+	}
+}
