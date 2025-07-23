bb95e3601d2fcb822c0d6032e6c1f87b9eaf8afc
Deon <deonsadcopen@outlook.com>
2025-07-23 14:40:33 +0800

    Add initial controller.

---
+++ ExpenseTrackerCoreMVC/Controllers/ExpenseController.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseTrackerCoreMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerCoreMVC.Controllers
+{
+	public class ExpenseController : Controller
+	{
+		// Readonly field to store the injected database context
+		private readonly ExpenseTrackerCoreMVCDbContext _context;
+
+		// Constructor that receives the DbContext via Dependency Injection
+		public ExpenseController(ExpenseTrackerCoreMVCDbContext context)
+		{ 
+			_context = context; // Assign the injected context to the private field
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
