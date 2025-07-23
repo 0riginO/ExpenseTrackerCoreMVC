517a6e9b540b3a4ce9641ca8595985ef0d1f8f70
Deon <deonsadcopen@outlook.com>
2025-07-23 21:04:02 +0800

    Rename fiile.

--- ExpenseTrackerCoreMVC/Controllers/ExpensesController.cs
+++ ExpenseTrackerCoreMVC/Controllers/ExpensesController.cs
@@ -3,19 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseTrackerCoreMVC.Controllers
 {
-	public class ExpenseController : Controller
+	public class ExpensesController : Controller
 	{
 		// Readonly field to store the injected database context
 		private readonly ExpenseTrackerCoreMVCDbContext _context;
 
 		// Constructor that receives the DbContext via Dependency Injection
-		public ExpenseController(ExpenseTrackerCoreMVCDbContext context)
+		public ExpensesController(ExpenseTrackerCoreMVCDbContext context)
 		{ 
 			_context = context; // Assign the injected context to the private field
 		}
 		public IActionResult Index()
 		{
-			return View();
+			var expenses = _context.Expenses.ToList();
+			return View(expenses);
 		}
 	}
 }
