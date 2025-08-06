ed0324bbdd7a52f1fe5a22293a0d7c483af85d7a
Deon <deonsadcopen@outlook.com>
2025-08-06 14:22:48 +0800

    Add CategoriesService to manage category data operations

---
+++ ExpenseTrackerCoreMVC/Data/Services/CategoriesService.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTrackerCoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerCoreMVC.Data.Services
+{
+	public class CategoriesService : ICategoriesServices
+	{
+		private readonly ExpenseTrackerCoreMVCDbContext _context;
+		public CategoriesService(ExpenseTrackerCoreMVCDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+		{
+			var categories = await _context.Categories.ToListAsync();
+			return categories;
+		}
+		public async Task Add(Category category)
+		{
+			await _context.Categories.AddAsync(category);
+			await _context.SaveChangesAsync();
+		}
+	}
+}
