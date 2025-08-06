9a0ecfde2524e282dc36e2695cc09eb60333a8d1
Deon <deonsadcopen@outlook.com>
2025-08-06 14:22:43 +0800

    Add ICategoriesServices interface for category management.

---
+++ ExpenseTrackerCoreMVC/Data/Services/ICategoriesServices.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseTrackerCoreMVC.Models;
+
+namespace ExpenseTrackerCoreMVC.Data.Services
+{
+	public interface ICategoriesServices
+	{
+		Task<IEnumerable<Category>> GetAllCategoriesAsync();
+		Task Add(Category category);
+	}
+}
