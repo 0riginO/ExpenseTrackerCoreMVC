b99fe86d8c6cadf66f9955d4df37d94bd6df6b0d
Deon <deonsadcopen@outlook.com>
2025-08-21 15:57:09 +0800

    Create View Model.

---
+++ ExpenseTrackerCoreMVC/ViewModels/ExpenseAddEditFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseTrackerCoreMVC.Models;
+
+namespace ExpenseTrackerCoreMVC.ViewModels
+{
+	public class ExpenseAddEditFormViewModel
+	{
+		public Expense Expense { get; set; }
+		public IEnumerable<Category> Categories { get; set; }
+	}
+}
