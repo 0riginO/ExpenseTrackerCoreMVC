6deb87643d25769664bca54023d21a89b58614f4
Deon <deonsadcopen@outlook.com>
2025-08-21 15:59:36 +0800

    Update Database.

--- ExpenseTrackerCoreMVC/Migrations/20250811054610_Update Expense.Designer.cs
+++ ExpenseTrackerCoreMVC/Migrations/20250811054610_Update Expense.Designer.cs
@@ -4,6 +4,7 @@ using ExpenseTrackerCoreMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpenseTrackerCoreMVC.Migrations
 {
     [DbContext(typeof(ExpenseTrackerCoreMVCDbContext))]
-    partial class ExpenseTrackerCoreMVCDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250811054610_Update Expense")]
+    partial class UpdateExpense
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
